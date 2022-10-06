7114dfe5524455616bb2a58ca9ecd2b35470f5ca
Asrafistcl2009 <asrafulcse1983@gmail.com>
2022-10-06 21:33:23 +0600

    ass2

---
+++ operator.py
@@ -0,0 +1,38 @@
+# ..........Assignment Operator .........
+
+# operator =
+
+x=8
+print(x)
+# result=8
+
+# operator +=
+y=6
+y+=x
+print(y)
+# result=14
+
+# operator -=
+y-=x
+print(y)
+#result=6
+
+# operator *=
+y*=x
+print(y)
+#result=48
+
+# operator /=
+y/=x
+print(y)
+#result=6.0
+
+# operator %=
+y%=x
+print(y)
+#result=6.0
+
+# operator **=
+y**=x
+print(y)
+#result=1679616
