f104197ab0c44eeac2fbcd82dc56a0bcddf2d9af
Asrafistcl2009 <asrafulcse1983@gmail.com>
2022-10-06 20:44:24 +0600

    home work

---
+++ asraf.py
@@ -0,0 +1,30 @@
+# a=2
+# b=3
+# print(a+b)
+
+# a=1
+# b=6
+# print(a+b)
+# x='5'
+# y=float(x)
+# print(type(y))
+
+
+# c=2
+# d=5
+# c-=d
+# print(c)
+a=5
+r=3
+a+=r
+print(a)
+a-=r
+print(a)
+x=10
+y=20
+x-=y
+print(x)
+
+c=10
+c&=4
+print(c)
---
+++ class2.py
@@ -0,0 +1,39 @@
+#Arithmetic Operator
+
+# addition
+num1=8
+num2=15
+num3=num1+num2
+print(num3)
+# result=23
+
+# Dedaction
+num4=35
+num5=num3-num4
+print(num5)
+#result=-12
+
+#Multiplication
+num6=num1*num2
+print(num6) 
+# result= 120
+
+#Divided
+num7=num1/num2
+print(num7)
+#result=0.5333
+
+#Modulus
+num8=num2%num1
+print(num8)
+# result=7
+
+#Exponentiation
+num9=num1**num2
+print(num9)
+# result=35184372088832
+
+#Floor division
+num10=num4//num1
+print(num10)
+#result=4
